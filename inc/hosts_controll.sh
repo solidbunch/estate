8d14e401cbb9a261653c4131c63a8ddac561c0b1
Yurii Pavlov <mailyurik@gmail.com>
2022-01-10 00:20:18 +0200

    adding all docker hosts to hosts file

--- inc/hosts_controll.sh
+++ inc/hosts_controll.sh
@@ -1,7 +1,34 @@
-# Edit hosts file records. Add new and remove unused
+# Edit hosts file records. Add new one and remove unused
+
+# WSL
+
+#echo "$ESTATE_DIR"
+
+add_all_hosts() {
+
+  echo '# Start Estate Docker Hosts' > estate_hosts.tmp
+
+  for dir in $(find "$APPS_DIR" -maxdepth 1 -type d -follow -print | sort -V); do
+
+    if [ "$dir" != "$APPS_DIR" ]; then
+      echo "127.0.0.1	"$(basename "$dir") >> estate_hosts.tmp
+    fi
+
+  done
+
+  echo '# End Estate Docker Hosts' >> estate_hosts.tmp
+
+  cat estate_hosts.tmp hosts >hosts.tmp
+
+  rm estate_hosts.tmp
+
+  mv hosts.tmp hosts
 
 
-echo "$ESTATE_DIR"
+}
+
+#sed -e 's/# Start Estate Docker Hosts\(.*\)# End Estate Docker Hosts/\1/ ' hosts > hosts.tt
+sed -z '/# Start Estate Docker Hosts\n\(.*\)\# End Estate Docker Hosts/p' hosts > hosts.tt
 
 
 echo -e "${LIGHTGREEN}[Success]${WHITE} root .env ready for ${NOCOLOR}" >&3
